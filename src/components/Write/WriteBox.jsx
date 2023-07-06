cf8d2937a68fa9d86c7e572e63436fd129536655
seoyeon-jung <jungluchia1204@gmail.com>
2023-07-06 17:19:23 +0900

    design: 글쓰기 Box UI 생성 #12

---
+++ src/components/Write/WriteBox.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const WriteBox = () => {
+  return (
+    <div>
+      <div>
+        강의명
+        <input />
+      </div>
+
+      <div>
+        교수 <input />
+      </div>
+
+      <div>별점 선택</div>
+
+      <div>
+        강의 평가
+        <textarea />
+      </div>
+
+      <div>
+        <button>등록</button>
+      </div>
+    </div>
+  );
+};
+
+export default WriteBox;
