de2299ed51c56ab7b024598ec67dc27defe7bc0c
seoyeon-jung <jungluchia1204@gmail.com>
2023-07-06 18:00:10 +0900

    style: WriteBox css 추가 #12
    - 별점 부분은 아직 구현하지 않았습니다
    - 강의명, 교수님은 input을 사용하였고 강의 평가는 textarea를 사용했습니다

--- src/components/Write/WriteBox.jsx
+++ src/components/Write/WriteBox.jsx
@@ -2,28 +2,75 @@ import styled from "styled-components";
 
 const WriteBox = () => {
   return (
-    <div>
+    <Wrapper>
       <div>
-        강의명
-        <input />
+        <Label>강의명</Label>
+        <Input placeholder="강의명을 입력하세요" />
       </div>
 
       <div>
-        교수 <input />
-      </div>
-
-      <div>별점 선택</div>
-
-      <div>
-        강의 평가
-        <textarea />
+        <Label>교수</Label>
+        <Input placeholder="교수님 이름을 입력하세요" />
       </div>
 
       <div>
-        <button>등록</button>
+        <Label>별점 선택</Label>
+        <Input />
       </div>
-    </div>
+
+      <div>
+        <Label>강의 평가</Label>
+        <Textarea placeholder="강의에 대한 자세한 평가를 해주세요 (최대 5줄 가능)" />
+      </div>
+
+      <div>
+        <Button>등록</Button>
+      </div>
+    </Wrapper>
   );
 };
+
+export const Wrapper = styled.div`
+  border: 1px solid #0c2e86;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px;
+  max-width: fit-content;
+  margin: 0 auto;
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
+
+export const Label = styled.div`
+  text-align: left;
+`;
+
+export const Input = styled.input`
+  text-align: left;
+  width: 600px;
+  height: 20px;
+  margin-top: 10px;
+  margin-bottom: 30px;
+  padding: 5px;
+`;
+
+export const Textarea = styled.textarea`
+  text-align: left;
+  width: 600px;
+  height: 200px;
+  margin-top: 10px;
+  margin-bottom: 30px;
+  padding: 5px;
+`;
+
+export const Button = styled.button`
+  background-color: #011445;
+  color: #ffffff;
+  padding: 5px;
+  width: 150px;
+  //border-radius: 10px;
+`;
 
 export default WriteBox;
