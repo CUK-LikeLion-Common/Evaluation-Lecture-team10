42fcb9850d9acc4c949aba591199fc1beffcdc60
seoyeon-jung <jungluchia1204@gmail.com>
2023-07-15 21:02:11 +0900

    feat: DetailPage 기능 구현 #75
    - api 명세서대로 detail page를 변경했습니다

--- src/components/Detail/DetailBox.jsx
+++ src/components/Detail/DetailBox.jsx
@@ -1,13 +1,54 @@
-import { FaStar } from "react-icons/fa";
 import { AiTwotoneLike } from "react-icons/ai";
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const DetailBox = () => {
-  // 추천 버튼 클릭시 나오는 alert
-  const ReviewBtn = () => {
-    alert("추천되었습니다!");
-    // 추후에 confirm 창으로 취소/확인 버튼으로 수정할 예정
+  const { evaluationID } = useParams();
+  const [evaluation, setEvaluation] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`/evaluation/read/${evaluationID}`)
+      .then((response) => {
+        setEvaluation(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("글을 불러오는데 실패했습니다.");
+      });
+  }, [evaluationID]);
+
+  if (!evaluation) {
+    return <p>loading...</p>;
+  }
+
+  //console.log(evaluation.result.message);
+
+  // 글 추천
+  const handleRecommendation = () => {
+    const requestData = {
+      userID: "",
+      userPassword: "",
+      userEmail: "",
+    };
+
+    axios
+      .post(`/likey/${evaluationID}`, requestData)
+      .then((response) => {
+        if (response.data.success) {
+          alert("글이 추천되었습니다!");
+        } else {
+          alert("글 추천에 실패했습니다.");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("글 추천에 실패했습니다.");
+      });
   };
+
   return (
     <Wrapper>
       <ButtonContainer>
@@ -15,50 +56,78 @@ const DetailBox = () => {
         <Button>삭제</Button>
       </ButtonContainer>
 
-      <LabelWrapper>
-        <Label>영화로 보는 한국사</Label>
-        <ProfessorWrapper>장미애</ProfessorWrapper>
-      </LabelWrapper>
-
-      <StarWrapper>
-        <StarLabel>별점 (4.0)</StarLabel>
-        <FaStar size="20" color="#fcc419" />
-        <FaStar size="20" color="#fcc419" />
-        <FaStar size="20" color="#fcc419" />
-        <FaStar size="20" color="#fcc419" />
-        <FaStar size="20" color="gray" />
-      </StarWrapper>
+      <div>
+        <Label>{evaluation.result.message.lectureName}</Label>
+        <ProfessorWrapper>
+          {evaluation.result.message.professorName}
+        </ProfessorWrapper>
+      </div>
 
       <div>
-        <DetailLabel>강의 평가</DetailLabel>
-        <DetailWrapper>
-          과제가 없어서 꿀 강의긴 한데 시험이 진짜 어렵습니다. 중간도 어렵고
-          기말은 더 어려워요. 3시간 풀강하시고 가끔 넘어서까지
-          수업하십니다...보여주시는 영화도 재밌고 수업도 열정적이시긴 합니다.
-          한국사에 좀 관심 많고 수업만 제대로 들어도 성적 잘 나옵니다. (저도
-          그랬어요) 시험 문제에 영화 장면 캡처해서 나오니까 주의하세요!
-        </DetailWrapper>
+        <div>수강 연도</div>
+        <div>{evaluation.result.message.lectureYear}</div>
+      </div>
+
+      <div>
+        <div>학기</div>
+        <div>{evaluation.result.message.semesterDivide}</div>
+      </div>
+
+      <div>
+        <div>강의 구분</div>
+        <div>{evaluation.result.message.lectureDivide}</div>
+      </div>
+
+      <div>
+        <div>평가 글 제목</div>
+        <div>{evaluation.result.message.evaluationTitle}</div>
+      </div>
+
+      <div>
+        <div>내용</div>
+        <div>{evaluation.result.message.evaluationContent}</div>
+      </div>
+
+      <div>
+        <div>총 점수</div>
+        <div>{evaluation.result.message.totalScore}</div>
+      </div>
+
+      <div>
+        <div>성적</div>
+        <div>{evaluation.result.message.creditScore}</div>
+      </div>
+
+      <div>
+        <div>강의가 어느정도 널널한가?</div>
+        <div>{evaluation.result.message.comfortableScrore}</div>
+      </div>
+
+      <div>
+        <div>평가 점수</div>
+        <div>{evaluation.result.message.lectureScore}</div>
       </div>
 
       <ReviewWrapper>
         <AiTwotoneLike size="20" color="#b9ccfa" />
-        <ReviewText onClick={() => ReviewBtn()}>추천</ReviewText>
+        <ReviewText onClick={handleRecommendation}>추천</ReviewText>
       </ReviewWrapper>
     </Wrapper>
   );
 };
 
-export const Wrapper = styled.div`
+const Wrapper = styled.div`
   border: 1px solid #0c2e86;
   text-align: center;
   display: flex;
   flex-direction: column;
-  //align-items: center;
+  align-items: center;
   padding: 30px;
+  width: 100%;
   max-width: fit-content;
   margin: 0 auto;
-  margin-top: 30px;
-  margin-bottom: 30px;
+  margin-top: 100px;
+  margin-bottom: 70px;
 `;
 
 export const ButtonContainer = styled.div`
@@ -76,13 +145,7 @@ export const Button = styled.div`
   }
 `;
 
-const LabelWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-`;
-
-export const Label = styled.div`
+const Label = styled.div`
   text-align: left;
   margin-bottom: 20px;
   font-size: 18px;
@@ -90,36 +153,13 @@ export const Label = styled.div`
   margin-right: 40px;
 `;
 
-export const ProfessorWrapper = styled.div`
+const ProfessorWrapper = styled.div`
   text-align: left;
   margin-bottom: 20px;
   font-size: 16px;
 `;
 
-export const StarWrapper = styled.div`
-  text-align: left;
-  margin-bottom: 30px;
-`;
-
-export const StarLabel = styled.div`
-  text-align: left;
-  margin-bottom: 10px;
-  font-size: 18px;
-`;
-
-export const DetailLabel = styled.div`
-  text-align: left;
-  margin-bottom: 10px;
-  font-size: 18px;
-  text-decoration: underline;
-  text-underline-position: under;
-`;
-
-export const DetailWrapper = styled.div`
-  text-align: left;
-`;
-
-export const ReviewWrapper = styled.div`
+const ReviewWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -132,7 +172,7 @@ export const ReviewWrapper = styled.div`
   width: 80px;
 `;
 
-export const ReviewText = styled.div`
+const ReviewText = styled.div`
   margin-left: 5px;
 `;
 
